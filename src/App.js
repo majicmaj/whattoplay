2286584498da1de0779eaecfd10fd16b8ea3ec9e
Majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-07-15 18:24:30 -0400

    version 1

--- src/App.js
+++ src/App.js
@@ -18,34 +18,32 @@ export default function App() {
   });
   const key = process.env.KEY || "NOKEY";
 
-  useEffect(
-    () => () => {
-      console.log(player1);
-      axios
-        .get(
-          "https://cors-anywhere.herokuapp.com/" +
-            "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" +
-            key +
-            "&steamid=" +
-            player1 +
-            "&format=json"
-        )
-        .then(r => {
-          setUserData(r.response);
-          console.log("returned!");
-          console.log(r);
-        })
-        .catch(err => {
-          console.error("error: " + err);
-        });
-    },
-    [key, player1]
-  );
+  useEffect(() => {
+    console.log("here");
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/" +
+          "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" +
+          key +
+          "&steamid=" +
+          player1 +
+          "&format=json"
+      )
+      .then(r => {
+        setUserData(r.response);
+        console.log("returned!");
+        console.log(r);
+      })
+      .catch(err => {
+        console.error("error: " + err);
+      });
+  }, [key, player1]);
 
   return (
     <div className="App">
       <div className="player">
         <h1>player 1:</h1>
+        <p>version: 1</p>
         <p>id: {player1}</p>
         <button onClick={() => setPlayer1("76561197960434622")}>
           NO CLICKY
